d614c6b31cf3f5f333a0b0199cba35b206f86974
Psibari <pdibari@gmail.com>
2026-05-31 21:08:04 -0400

    d

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
-  };
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-private-methods',
+    '@babel/plugin-proposal-private-property-in-object',
+    'react-native-reanimated/plugin',
+  ],
 };
